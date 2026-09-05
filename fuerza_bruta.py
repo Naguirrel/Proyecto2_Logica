854497181a4f6c6be0ce0721c5c2a77381ea778f
Norman <agu24479@uvg.edu.gt>
2026-09-04 23:54:33 -0600

    Agrega metricas de tiempo y combinaciones

--- fuerza_bruta.py
+++ fuerza_bruta.py
@@ -3,13 +3,19 @@ from itertools import product
 from utilidades import evaluar_formula, obtener_variables
 
 
-def fuerza_bruta(formula):
+def fuerza_bruta(formula, devolver_estadisticas=False):
     """Resuelve una formula CNF probando todas las asignaciones posibles."""
     variables = obtener_variables(formula)
+    estadisticas = {"combinaciones_probadas": 0}
 
     for valores in product([False, True], repeat=len(variables)):
+        estadisticas["combinaciones_probadas"] += 1
         asignacion = dict(zip(variables, valores))
         if evaluar_formula(formula, asignacion):
+            if devolver_estadisticas:
+                return True, asignacion, estadisticas
             return True, asignacion
 
+    if devolver_estadisticas:
+        return False, None, estadisticas
     return False, None
